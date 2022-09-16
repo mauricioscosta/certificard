6a44a9a4eb1478b0883634e9bf94e8e49fdb71e7
mauricioscosta <113733402+mauricioscosta@users.noreply.github.com>
2022-09-16 20:14:31 -0300

    Upload Teste Certificard

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+UI/UX
+Figma
+AdobeXD
+Sketch
+Zeppelin
+
+Editores similares ao CodePen:
+VisualCode
+Repositório de emojis: Bag Emojis
+*/
+
+/*
+Desafios:
+Terminar o layout do figma no html e CSS;
+Links clicáveis com os perfis de rede sociais
+Buscar hashtags imersaodev no codePen
+
+
+*/
+
+function alterarTema() {
+  document.body.classList.toggle("tema-escuro");
+}
